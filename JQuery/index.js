3ddba7a531f95f9a5176d6d2cca03fa74eb62d89
Prashant Singh <psingh5025615@gmail.com>
2022-11-18 21:54:47 +0530

    initial commit

--- JQuery/index.js
+++ JQuery/index.js
@@ -1,8 +1,17 @@
 // $("div").add("p").css("color", "red").addClass("red");
 // $("red")
 
-$("#button").click(function() {
-    $("#red").slideDown("slow", function() {
+// $("h1").css("color", "red").addClass("red");
 
-    })
-})
+// $("h1").text("Bye").addClass("red");
+
+// $("button").on("click", function() {
+//     $("p").slideDown().slideUp().animate({opacity: 0.5
+    
+//     });
+
+// });
+
+
+$("p").text("New Text");
+
