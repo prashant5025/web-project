26938bffb13deb2a3c56d087bdccd3f83e739e5f
Prashant Singh <psingh5025615@gmail.com>
2022-12-08 17:25:28 +0530

    initial commit

--- react-course/lesson-01/src/components/Expenses/Expenses.jsx
+++ react-course/lesson-01/src/components/Expenses/Expenses.jsx
@@ -6,17 +6,25 @@ import "./Expenses.css";
 import { useState } from "react";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2022");
 
-    const [filteredYear, setFilteredYear] = useState('2020');
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
 
-    const filterChangeHandler = (selectedYear) => {
-            setFilteredYear(selectedYear);
-    }
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  console.log(filteredExpenses);
 
   return (
     <div>
       <Card className="expenses">
-        <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+        <ExpenseFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
         {props.items.map((expense, key) => {
           return (
             <ExpenseItem
