f962e04cd78b5416ff6da46608efa9a566f999c9
Prashant Singh <psingh5025615@gmail.com>
2022-12-12 23:43:08 +0530

    initial commit

--- react-course/lesson-02/src/components/UI/Button/Button.js
+++ react-course/lesson-02/src/components/UI/Button/Button.js
@@ -1,9 +1,7 @@
-import React from 'react';
 import styled from 'styled-components';
 import './Button.css';
 
 const Button = styled.button`
-  width: 100%;
   font: inherit;
   padding: 0.5rem 1.5rem;
   border: 1px solid #8b005d;
@@ -11,9 +9,17 @@ const Button = styled.button`
   background: #8b005d;
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
   cursor: pointer;
-  
 
+  &: focus {
+    outline: none;
+  }
 
+  &: hover,
+  &: active {
+    background: #ac0e77;
+    border-color: #ac0e77;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
 `;
 
 // const Button = props => {
