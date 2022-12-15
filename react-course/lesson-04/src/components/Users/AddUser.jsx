2f1c4873d5449e650206bcf6a0af51c09bef3dd2
Prashant Singh <psingh5025615@gmail.com>
2022-12-15 23:49:40 +0530

    initial commit

--- react-course/lesson-04/src/components/Users/AddUser.jsx
+++ react-course/lesson-04/src/components/Users/AddUser.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import Card from '../UI/Card'
 
+import classes from './AddUser.module.css'
+
 const AddUser = (props) => {
     const addUserHandler = (event) => {
         event.preventDefault();
     }
   return (
-    <Card>
+    <Card className={classes.input}>
     <form onSubmit={addUserHandler}>
-        <label className='' htmlFor="username">Username</label>
+        <label  htmlFor="username">Username</label>
         <input type="text" name="username" id="username" />
         <label className='' htmlFor="age">Age (Year)</label>
         <input type="number" name="username" id="age" />
