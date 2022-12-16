4afcfb6ce49cfdd39975f5c81a3fa2e5f437152f
Prashant Singh <psingh5025615@gmail.com>
2022-12-16 20:03:25 +0530

    initial commit

--- react-course/lesson-04/src/components/Users/AddUser.jsx
+++ react-course/lesson-04/src/components/Users/AddUser.jsx
@@ -1,24 +1,50 @@
-import React from 'react'
-import Button from '../UI/Button'
-import Card from '../UI/Card'
+import React, { useState } from "react";
+import Button from "../UI/Button";
+import Card from "../UI/Card";
 
-import classes from './AddUser.module.css'
+import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
-    const addUserHandler = (event) => {
-        event.preventDefault();
-    }
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
+  const usernameChangeHandler = (e) => {
+    setEnteredUsername(e.target.value);
+  };
+
+  const ageChangeHandler = (e) => {
+    setEnteredAge(e.target.value);
+  };
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    console.log(enteredUsername, enteredAge);
+  };
   return (
     <Card className={classes.input}>
-    <form onSubmit={addUserHandler}>
-        <label  htmlFor="username">Username</label>
-        <input type="text" name="username" id="username" placeholder='Enter Your Name'/>
-        <label className='' htmlFor="age">Age (Year)</label>
-        <input type="number" name="username" id="age" placeholder='Enter Your Age'/>
-        <Button type='submit'>Add User</Button>
-    </form>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          onChange={usernameChangeHandler}
+          placeholder="Enter Your Name"
+        />
+        <label className="" htmlFor="age">
+          Age (Year)
+        </label>
+        <input
+          type="number"
+          name="username"
+          id="age"
+          onChange={ageChangeHandler}
+          placeholder="Enter Your Age"
+        />
+        <Button type="submit">Add User</Button>
+      </form>
     </Card>
-  )
-}
+  );
+};
 
-export default AddUser
+export default AddUser;
