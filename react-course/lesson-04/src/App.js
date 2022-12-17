dfb69680531deb2c705cb645b6fbd4026affb7dd
Prashant Singh <psingh5025615@gmail.com>
2022-12-17 23:39:19 +0530

    initial commit

--- react-course/lesson-04/src/App.js
+++ react-course/lesson-04/src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddUser from './components/Users/AddUser';
+import UserList from './components/Users/UserList';
 
 
 
 function App() {
+  const [userList, setUserList] = useState([]);
   const addUserHandler = (userName, userAge) => {
-    console.log(userName, userAge);
+    setUserList((prevUserList) => {
+      return [...prevUserList, {name: userName, age: userAge, id: Math.random().toString()}]
+    });
   }
   return (
     <div>
       <AddUser onAddUser={addUserHandler}/>
+      <UserList users={userList}/>
+
     </div>
   );
 }
