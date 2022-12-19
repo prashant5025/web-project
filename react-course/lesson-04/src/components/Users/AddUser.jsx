619842941f649b67101473618aa45065d922841c
Prashant Singh <psingh5025615@gmail.com>
2022-12-19 22:00:02 +0530

    initial commit

--- react-course/lesson-04/src/components/Users/AddUser.jsx
+++ react-course/lesson-04/src/components/Users/AddUser.jsx
@@ -24,16 +24,16 @@ const AddUser = (props) => {
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
       setError({
         title: "Invalid input",
-        message: "Please enter a valid name and age (non-empty values)."
-      })
+        message: "Please enter a valid name and age (non-empty values).",
+      });
       return;
     }
 
     if (+enteredAge < 1) {
       setError({
         title: "Invalid age",
-        message: "Please enter a valid age (> 0)."
-      })
+        message: "Please enter a valid age (> 0).",
+      });
       return;
     }
 
@@ -45,11 +45,16 @@ const AddUser = (props) => {
 
   const errorHandler = () => {
     setError(null);
-
-  }
+  };
   return (
     <>
-      {error && (<ErrorModel title={error.title} message={error.message} onConfirm={errorHandler}/>)}
+      {error && (
+        <ErrorModel
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
