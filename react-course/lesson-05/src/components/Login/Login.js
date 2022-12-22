76deace09b798795b3c8c56fba39204b589f14d1
Prashant Singh <psingh5025615@gmail.com>
2022-12-22 08:18:50 +0530

    initial commit

--- react-course/lesson-05/src/components/Login/Login.js
+++ react-course/lesson-05/src/components/Login/Login.js
@@ -12,6 +12,10 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
+    console.log('EFFECT RUNNING');
+  }, [enteredEmail])
+
+  useEffect(() => {
     const identifier = setTimeout(() => {
       console.log('Checking form validity!');
       setFormIsValid(
