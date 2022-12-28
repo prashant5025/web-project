16668251bd9e3c4805dce0eac24990d77b2beb1a
Prashant Singh <psingh5025615@gmail.com>
2022-12-28 08:10:19 +0530

    initial commit

--- react-course/lesson-05/src/components/Login/Login.js
+++ react-course/lesson-05/src/components/Login/Login.js
@@ -38,8 +38,8 @@ const passwordReducer = (state, action) => {
 const Login = (props) => {
   // const [enteredEmail, setEnteredEmail] = useState('');
   // const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState('');
-  const [passwordIsValid, setPasswordIsValid] = useState();
+  // const [enteredPassword, setEnteredPassword] = useState('');
+  // const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
   const [emailState, dispatchEmail] = useReducer(emailReducer, {
@@ -47,35 +47,44 @@ const Login = (props) => {
     isValid: null
   });
 
+  const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
+    value: '',
+    isValid: null
+  })
+
   
 
   // useEffect(() => {
   //   console.log('EFFECT RUNNING');
   // }, [enteredEmail])
 
-  // useEffect(() => {
-  //   const identifier = setTimeout(() => {
-  //     console.log('Checking form validity!');
-  //     setFormIsValid(
-  //       enteredEmail.includes('@') && enteredPassword.trim().length > 6
-  //     );
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      console.log('Checking form validity!');
+      setFormIsValid(
+        emailState.isValid.includes('@') && passwordState.isValid
+      );
       
-  //   }, 500);
+    }, 500);
 
-  //   return () => {
-  //     console.log('CLEANUP');
-  //     clearTimeout(identifier);
+    return () => {
+      console.log('CLEANUP');
+      clearTimeout(identifier);
 
-  //   }
-  // },[setFormIsValid, enteredPassword])
+    }
+  },[emailState, passwordState])
 
   const emailChangeHandler = (event) => {
     dispatchEmail({type: ACTION.USER_INPUT, val: event.target.value});
 
+    // setFormIsValid(
+    //   event.target.value.includes('@') && passwordState.isValid
+    // );
+
   };
 
   const passwordChangeHandler = (event) => {
-    setEnteredPassword(event.target.value);
+    dispatchPassword({type: ACTION.USER_INPUT, val: event.target.value})
 
     setFormIsValid(
       emailState.isValid && event.target.value.trim().length > 6
@@ -87,12 +96,12 @@ const Login = (props) => {
   };
 
   const validatePasswordHandler = () => {
-    setPasswordIsValid(enteredPassword.trim().length > 6);
+    dispatchPassword({type: ACTION.INPUT_BLUR})
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, enteredPassword);
+    props.onLogin(emailState.value, passwordState.value);
   };
 
   return (
@@ -114,14 +123,14 @@ const Login = (props) => {
         </div>
         <div
           className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ''
+            passwordState.isValid === false ? classes.invalid : ''
           }`}
         >
           <label htmlFor="password">Password</label>
           <input
             type="password"
             id="password"
-            value={enteredPassword}
+            value={passwordState.value}
             onChange={passwordChangeHandler}
             onBlur={validatePasswordHandler}
           />
